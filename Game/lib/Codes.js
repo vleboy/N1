8875246c310e7b481de08a3484d9ea040f5e9e26
cheneyweb <457299596@qq.com>
2019-04-03 11:32:24 +0800

    Game精简

--- Game/lib/Codes.js
+++ Game/lib/Codes.js
@@ -1,17 +1,8 @@
 const Codes = {
   OK: '0',
-  Error: '-1',
-
-  DBError: '50001'
+  Error: '-1'
 }
-const BizErr = {
-  DBErr: (errMsg = 'DBError') => {
-    return { code: Codes.DBError, msg: errMsg }
-  }
-}
-
 
 module.exports = {
-  Codes,
-  BizErr
+  Codes
 }
