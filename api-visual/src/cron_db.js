2272b1e7ce4bf2c32f2541e84fa1c7534e33d285
xj <799137494@qq.com>
2019-04-09 17:12:24 +0800

    定时服务

--- api-visual/src/cron_db.js
+++ api-visual/src/cron_db.js
@@ -12,7 +12,6 @@ function queryInc(params, result) {
             result.Items.push(...res.Items)
         }
         if (res.LastEvaluatedKey) {
-            console.log(111)
             params.ExclusiveStartKey = res.LastEvaluatedKey
             return queryInc(params, result)
         } else {
@@ -27,31 +26,43 @@ function queryInc(params, result) {
 cron.schedule('*/30 * * * * *', async () => {
     let configArr = await nodebatis.query('config.findOne', { type: 'queryTime' })
     if (configArr[0].flag) {
+        // 读取
+        await nodebatis.execute('config.updateFlag', { type: 'queryTime', flag: 0 })
         let startTime = configArr[0].createdAt
-        await nodebatis.execute('config.updateFlag', { type: 'queryTime', flag: 'false' })
         let endTime = startTime + 24 * 60 * 60 * 1000 > Date.now() ? Date.now() : startTime + 24 * 60 * 60 * 1000
-        let result = await queryInc({
-            TableName: 'PlayerBillDetail',
-            IndexName: 'TypeIndex',
-            KeyConditionExpression: '#type=:type AND createdAt BETWEEN :createdAt1 AND :createdAt2',
-            ProjectionExpression: 'sn,businessKey,parent,userId,userName,gameType,gameId,#type,originalAmount,amount,balance,sourceIP,createdAt',
-            ExpressionAttributeNames: {
-                '#type': 'type'
-            },
-            ExpressionAttributeValues: {
-                ':type': 3,
-                ':createdAt1': startTime,
-                ':createdAt2': endTime
-            }
-        })
-        //插入数据
-        console.log(`写入数据共${result.Items.length}条`)
-        if (result.Items.length != 0) {
-            await nodebatis.execute('bill.batchInsert', { data: result.Items })
+        let promiseReadArr = []
+        for (let i = 3; i <= 5; i++) {
+            promiseReadArr.push(queryInc({
+                TableName: 'PlayerBillDetail',
+                IndexName: 'TypeIndex',
+                KeyConditionExpression: '#type=:type AND createdAt BETWEEN :createdAt1 AND :createdAt2',
+                ProjectionExpression: 'sn,businessKey,parent,userId,userName,gameType,gameId,#type,originalAmount,amount,balance,sourceIP,createdAt',
+                ExpressionAttributeNames: {
+                    '#type': 'type'
+                },
+                ExpressionAttributeValues: {
+                    ':type': i,
+                    ':createdAt1': startTime,
+                    ':createdAt2': endTime
+                }
+            }))
         }
-        await nodebatis.execute('config.updateOne', { type: 'queryTime', createdAt: endTime, flag: 'true' })
+        let resArr = await Promise.all(promiseReadArr)
+        // 写入
+        let promiseWriteArr = []
+        for (let res of resArr) {
+            if (res.Items.length > 0) {
+                promiseWriteArr.push(nodebatis.execute('bill.batchInsert', {
+                    data: res.Items.map((item) => {
+                        item.sourceIP = item.sourceIP || '0.0.0.0'
+                        return item
+                    })
+                }))
+            }
+        }
+        await Promise.all(promiseWriteArr)
+        await nodebatis.execute('config.updateOne', { type: 'queryTime', createdAt: endTime, flag: 1 })
     }
-
 })
 
 
