92a41e19460e449842fd012fb4f83bf9daf6c23d
xj <799137494@qq.com>
2019-04-17 12:07:35 +0800

    柱状图

--- api-visual/src/api_line.js
+++ api-visual/src/api_line.js
@@ -10,7 +10,7 @@ const _ = require('lodash')
 const log = require('tracer').colorConsole({ level: config.log.level })
 
 //当天全局折线图map
-let lineDay = {
+let lineMap = {
     playerCount: [],
     betCount: [],
     betAmount: [],
@@ -19,10 +19,51 @@ let lineDay = {
     winloseAmount: []
 }
 
+
 //五分钟全局对象统计输赢金额
 let winloseMap = {
     winloseAmount: []
 }
+
+//时间段柱状图
+router.get('/line/graph', async (ctx, next) => {
+    console.time('柱状图统计耗时')
+    let inparam = ctx.request.query
+    let promiseArr = []
+    // 初始时间柱状图map
+    let GraphMap = {
+        playerCount: [],
+        betCount: [],
+        betAmount: [],
+        retAmount: [],
+        refundAmount: [],
+        winloseAmount: []
+    }
+    for (let i = 0; i < 24; i++) {
+        GraphMap.playerCount.push({ x: i, y: 0 })
+        GraphMap.betCount.push({ x: i, y: 0 })
+        GraphMap.betAmount.push({ x: i, y: 0 })
+        GraphMap.retAmount.push({ x: i, y: 0 })
+        GraphMap.refundAmount.push({ x: i, y: 0 })
+        GraphMap.winloseAmount.push({ x: i, y: 0 })
+    }
+    // 获取区域玩家总人数
+    promiseArr.push(queryGetGraph('bill.playerCountGraph', inparam, 'playerCount', GraphMap))
+    // 获取区域玩家总下注次数
+    promiseArr.push(queryGetGraph('bill.betCountGraph', inparam, 'betCount', GraphMap))
+    // 获取区域玩家总下注金额
+    promiseArr.push(queryGetGraph('bill.betAmountGraph', inparam, 'betAmount', GraphMap))
+    // 获取区域玩家总返奖
+    promiseArr.push(queryGetGraph('bill.retAmountGraph', inparam, 'retAmount', GraphMap))
+    // 获取区域玩家总退款
+    promiseArr.push(queryGetGraph('bill.refundAmountGraph', inparam, 'refundAmount', GraphMap))
+    // 获取区域玩家总输赢
+    promiseArr.push(queryGetGraph('bill.winloseAmountGraph', inparam, 'winloseAmount', GraphMap))
+    // 并发执行
+    await Promise.all(promiseArr)
+    ctx.body = { code: 0, data: GraphMap }
+    console.timeEnd('柱状图统计耗时')
+})
 
 /**
  *  当天这些图统计
@@ -33,22 +74,21 @@ router.get('/line/everyDay', async (ctx, next) => {
     inparam.startTime = new Date(new Date().toLocaleDateString()).getTime()  //获取当天零点时间
     inparam.endTime = Date.now() - 5 * 60 * 60 * 1000  //获取查询的时间
     let promiseArr = []
-
     // 获取区域玩家总人数
-    promiseArr.push(queryGetSql('bill.playerCountDay', inparam, 'playerCount'))
+    promiseArr.push(queryGetLine('bill.playerCountDay', inparam, 'playerCount'))
     // 获取区域玩家总下注次数
-    promiseArr.push(queryGetSql('bill.betCountDay', inparam, 'betCount'))
+    promiseArr.push(queryGetLine('bill.betCountDay', inparam, 'betCount'))
     // 获取区域玩家总下注金额
-    promiseArr.push(queryGetSql('bill.betAmountDay', inparam, 'betAmount'))
+    promiseArr.push(queryGetLine('bill.betAmountDay', inparam, 'betAmount'))
     // 获取区域玩家总返奖
-    promiseArr.push(queryGetSql('bill.retAmountDay', inparam, 'retAmount'))
+    promiseArr.push(queryGetLine('bill.retAmountDay', inparam, 'retAmount'))
     // 获取区域玩家总退款
-    promiseArr.push(queryGetSql('bill.refundAmountDay', inparam, 'refundAmount'))
+    promiseArr.push(queryGetLine('bill.refundAmountDay', inparam, 'refundAmount'))
     // 获取区域玩家总输赢
-    promiseArr.push(queryGetSql('bill.winloseAmountDay', inparam, 'winloseAmount'))
+    promiseArr.push(queryGetLine('bill.winloseAmountDay', inparam, 'winloseAmount'))
     // 并发执行
     await Promise.all(promiseArr)
-    ctx.body = { code: 0, data: lineDay }
+    ctx.body = { code: 0, data: lineMap }
     console.timeEnd('实时统计耗时')
 })
 
@@ -85,13 +125,24 @@ router.get('/line/winloseAmount', async (ctx, next) => {
 })
 
 
-// sql查询
-async function queryGetSql(sqlName, inparam, key, map) {
+// 折线图sql查询
+async function queryGetLine(sqlName, inparam, key) {
     let res = await nodebatis.query(sqlName, { startTime: inparam.startTime, endTime: inparam.endTime, gameType: inparam.gameType })
     if (res[0].total) {
-        map[key].push(res[0].total)
+        lineMap[key].push(res[0].total)
     } else {
-        map[key].push(0)
+        lineMap[key].push(0)
+    }
+}
+// 柱状图sql查询
+async function queryGetGraph(sqlName, inparam, key, map) {
+    let res = await nodebatis.query(sqlName, { startTime: inparam.startTime, endTime: inparam.endTime, gameType: inparam.gameType })
+    for (let item of res) {
+        for (let valueMap of map[key]) {
+            if (valueMap.x == item.days) {
+                valueMap.y = item.count
+            }
+        }
     }
 }
 
