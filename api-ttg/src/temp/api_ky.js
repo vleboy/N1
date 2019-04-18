c87c59ad0e1f1e1f47d8c98e3ad2a154fe21840d
cheneyweb <457299596@qq.com>
2019-04-18 14:10:03 +0800

    KY重启

--- api-ttg/src/temp/api_ky.js
+++ api-ttg/src/temp/api_ky.js
@@ -1,108 +1,139 @@
-// // 系统配置参数
-// const config = require('config')
-// // 路由相关
-// const Router = require('koa-router')
-// const router = new Router()
-// // 工具相关
-// const _ = require('lodash')
-// const axios = require('axios')
-// const moment = require('moment')
-// const crypto = require('crypto')
-// const qs = require('querystring')
-// // 日志相关
-// const log = require('tracer').colorConsole({ level: config.log.level })
-// // 持久层相关
-// const PlayerModel = require('./model/PlayerModel')
-// const PlayerBillDetailModel = require('./model/PlayerBillDetailModel')
-// const LogModel = require('./model/LogModel')
+// 系统配置参数
+const config = require('config')
+// 路由相关
+const Router = require('koa-router')
+const router = new Router()
+// 工具相关
+const _ = require('lodash')
+const axios = require('axios')
+const moment = require('moment')
+const crypto = require('crypto')
+const qs = require('querystring')
+// 日志相关
+const log = require('tracer').colorConsole({ level: config.log.level })
+// 持久层相关
+const PlayerModel = require('./model/PlayerModel')
+const ipMap = {}
 
-// /**
-//  * 开元棋牌玩家登录
-//  * @param account 玩家账号
-//  */
-// router.get('/ky/gameurl/:gameId/:sid/:userId/:token', async function (ctx, next) {
-//     let time = new moment().utcOffset(8)
-//     let inparam = ctx.params
-//     // 请求N1服务器是否允许玩家进入游戏
-//     const nares = await axios.post(config.na.joingameurl, {
-//         userId: inparam.userId,
-//         gameId: inparam.gameId,
-//         sid: inparam.sid,
-//         token: inparam.token
-//     })
-//     if (nares.data.code != 0) {
-//         ctx.body = { code: nares.data.code, msg: nares.data.msg }
-//         return
-//     }
-//     let account = inparam.userId                                                                //玩家账号
-//     //
-//     //获取玩家的余额
-//     const player = await new PlayerModel().getPlayerById(account)
-//     let money = player.balance                                                                  //玩家的余额
-//     let orderId = inparam.orderId || config.ky.agent + time.format("YYYYMMDDHHmmss") + account  //流水号
-//     let ip = ctx.request.ip                                                                     //玩家IP
-//     let lineCode = inparam.lineCode || 1                                                        //代理下面的站点标识
-//     let param = `s=0&account=${account}&money=${money}&lineCode=${lineCode}&ip=${ip}&orderid=${orderId}&KindId=0`
-//     let url = getURL(0, param)
-//     let response = await axios.get(url, { timeout: 100 * 1000 })
-//     if (response.data.d.code == 0) {
-//         //如果操作成功则（模拟）下注扣除玩家的上分（余额）
-//         let updateParams = {}
-//         updateParams.billType = 3
-//         updateParams.amt = parseFloat(money) * -1
-//         updateParams.gameType = config.ky.gameType
-//         updateParams.businessKey = `BKY_${account}_${orderId}`                        // 设置局号
-//         updateParams.userId = player.userId
-//         updateParams.userName = player.userName
-//         let amtAfter = await new PlayerModel().updatebalance(player, updateParams)
-//         if (amtAfter == 'err') {
-//             ctx.body = { code: 404, message: "发生错误了" }
-//         } else {
-//             ctx.redirect(response.data.d.url)
-//         }
-//     } else {
-//         ctx.body = { code: response.data.d.code, message: "上分失败", err: response.data.d }
-//     }
-// })
+/**
+ * 玩家上线
+ * @param {*} gameId NA游戏大类
+ * @param {*} sid NA游戏ID
+ * @param {*} userId NA玩家ID，0是试玩
+ * @param {*} token NA玩家TOKEN，0是试玩
+ * @param {*} lobbyType 0是电脑版，1是移动版 */
+router.get('/ky/gameurl/:gameId/:sid/:userId/:token', async (ctx, next) => {
+    let ip = ipMap[ctx.params.userId] = ctx.request.ip
+    const inparam = ctx.params
+    // 请求N1服务器是否允许玩家进入游戏
+    const nares = await axios.post(config.na.joingameurl, {
+        userId: inparam.userId,
+        gameId: inparam.gameId,
+        sid: inparam.sid,
+        token: inparam.token
+    })
+    if (nares.data.code != 0) {
+        return ctx.body = { code: nares.data.code, msg: nares.data.msg }
+    }
+    // 获取玩家的余额，全部上分
+    const account = inparam.userId                                                                                //玩家账号
+    const player = await new PlayerModel().getPlayerById(account)
+    const money = player.balance                                                                                  //玩家的余额
+    const orderId = inparam.orderId || config.ky.agent + moment().utcOffset(8).format("YYYYMMDDHHmmss") + account //流水号
+    const lineCode = inparam.lineCode || 1                                                                        //代理下面的站点标识
+    const param = `s=0&account=${account}&money=${money}&lineCode=${lineCode}&ip=${ip}&orderid=${orderId}&KindId=0`
+    const url = getURL(0, param)
+    const response = await axios.get(url, { timeout: 100 * 1000 })
+    if (response.data.d.code == 0) {
+        //如果操作成功则（模拟）下注扣除玩家的上分（余额）
+        const updateParams = {}
+        updateParams.billType = 3
+        updateParams.amt = money * -1
+        updateParams.gameType = config.ky.gameType
+        updateParams.businessKey = `BKY_${account}_${orderId}`
+        let amtAfter = await new PlayerModel().updatebalance(player, updateParams)
+        if (amtAfter == 'err') {
+            ctx.body = { code: 404, message: "发生错误了" }
+        } else {
+            ctx.redirect(response.data.d.url)
+        }
+    } else {
+        ctx.body = { code: response.data.d.code, message: "上分失败", err: response.data.d }
+    }
+})
 
-// /**
-//  * 查询/踢玩家下线接口
-//  * @param s 操作类型 1：查询玩家可下分余额 5：查询玩家是否在线 7：查询游戏总余额 8：根据玩家账号提玩家下线
-//  * @param account 玩家账号
-//  */
-// router.get('/ky/:s/:account', async function (ctx, next) {
-//     let time = new moment().utcOffset(8)
-//     //获取入参
-//     let inparam = ctx.params
-//     let account = inparam.account                                                               //会员账号
-//     let s = inparam.s                                                                           //操作子类型
-//     let param = `s=${s}&account=${account}`
-//     //获取请求url
-//     let url = getURL(parseInt(s), param)
-//     let response = await axios.get(url, { timeout: 100 * 1000 })
-//     //根据操作类型做相应处理
-//     if (response.data.d.code == 0) {
-//         switch (parseInt(s)) {
-//             case 1://查询玩家可下分余额
-//                 ctx.body = { code: 0, message: "success", money: response.data.d.money }
-//                 break;
-//             case 5://查询玩家是否在线
-//                 ctx.body = { code: 0, message: "success", status: response.data.d.status }
-//                 break;
-//             case 7://查询游戏总余额
-//                 ctx.body = { code: 0, message: "success", totalMoney: response.data.d.totalMoney, freeMoney: response.data.d.freeMoney }
-//                 break;
-//             case 8://根据玩家账号提玩家下线
-//                 //如果是踢玩家下线那么应该做些什么处理？？？？
-//                 //通知服务器，查询可下分余额并返奖等。。。。
-//                 //未完！！！
-//                 ctx.body = { code: 0, message: "success" }
-//                 break;
-//         }
-//     } else {
-//         ctx.body = { code: -1, msg: '操作失败', err: response.data }
-//     }
-// })
+/**
+ * 玩家下线（提供给ky的回调通知）
+ */
+router.get('/ky/logout', async (ctx, next) => {
+    //获取入参
+    let inparam = ctx.request.body
+    let agent = inparam.agent
+    let account = inparam.account
+    let money = inparam.money
+    //模拟返奖更新余额
+    const player = await new PlayerModel().getPlayerById(account)
+})
+
+/**
+ * 获取游戏注单（拉去时间最大不能超过60分钟）
+ */
+router.post('/ky/betdetail', async (ctx, next) => {
+    let time = new moment().utcOffset(8)
+    //获取入参
+    let inparam = ctx.request.body
+    let startTime = inparam.startTime                                                           //开始时间
+    let endTime = inparam.endTime                                                               //结束时间                                       
+    let param = param = `s=6&startTime=${startTime}&endTime=${endTime}`
+    //获取请求url
+    let url = getURL(6, param)
+    let response = await axios.get(url, { timeout: 100 * 1000 })
+    //根据操作类型做相应处理
+    if (response.data.d.code == 0) {
+
+    } else {
+        ctx.body = { code: -1, msg: '操作失败', err: response.data }
+    }
+})
+
+/**
+ * 查询/踢玩家下线接口
+ * @param s 操作类型 1：查询玩家可下分余额 5：查询玩家是否在线 7：查询游戏总余额 8：根据玩家账号提玩家下线
+ * @param account 玩家账号
+ */
+router.get('/ky/:s/:account', async (ctx, next) => {
+    let time = new moment().utcOffset(8)
+    //获取入参
+    let inparam = ctx.params
+    let account = inparam.account                                                               //会员账号
+    let s = inparam.s                                                                           //操作子类型
+    let param = `s=${s}&account=${account}`
+    //获取请求url
+    let url = getURL(parseInt(s), param)
+    let response = await axios.get(url, { timeout: 100 * 1000 })
+    //根据操作类型做相应处理
+    if (response.data.d.code == 0) {
+        switch (parseInt(s)) {
+            case 1://查询玩家可下分余额
+                ctx.body = { code: 0, message: "success", money: response.data.d.money }
+                break;
+            case 5://查询玩家是否在线
+                ctx.body = { code: 0, message: "success", status: response.data.d.status }
+                break;
+            case 7://查询游戏总余额
+                ctx.body = { code: 0, message: "success", totalMoney: response.data.d.totalMoney, freeMoney: response.data.d.freeMoney }
+                break;
+            case 8://根据玩家账号提玩家下线
+                //如果是踢玩家下线那么应该做些什么处理？？？？
+                //通知服务器，查询可下分余额并返奖等。。。。
+                //未完！！！
+                ctx.body = { code: 0, message: "success" }
+                break;
+        }
+    } else {
+        ctx.body = { code: -1, msg: '操作失败', err: response.data }
+    }
+})
 // /**
 //  * 上下分订单状态接口
 //  * @param orderId 流水号
@@ -175,39 +206,7 @@
 //         ctx.body = { code: -1, msg: '操作失败', err: response.data }
 //     }
 // })
-// /**
-//  * 获取游戏注单（拉去时间最大不能超过60分钟）
-//  */
-// router.post('/ky/getBillDetail', async function (ctx, next) {
-//     let time = new moment().utcOffset(8)
-//     //获取入参
-//     let inparam = ctx.request.body
-//     let startTime = inparam.startTime                                                           //开始时间
-//     let endTime = inparam.endTime                                                               //结束时间                                       
-//     let param = param = `s=6&startTime=${startTime}&endTime=${endTime}`
-//     //获取请求url
-//     let url = getURL(6, param)
-//     let response = await axios.get(url, { timeout: 100 * 1000 })
-//     //根据操作类型做相应处理
-//     if (response.data.d.code == 0) {
 
-//     } else {
-//         ctx.body = { code: -1, msg: '操作失败', err: response.data }
-//     }
-// })
-// /**
-//  * 玩家下线（提供给ky的回调通知）
-//  */
-// router.post('/ky/logout', async function (ctx, next) {
-//     //获取入参
-//     let inparam = ctx.request.body
-//     let agent = inparam.agent
-//     let account = inparam.account
-//     let money = inparam.money
-//     //模拟返奖更新余额
-//     const player = await new PlayerModel().getPlayerById(account)
-    
-// })
 
 
 // /**
@@ -242,38 +241,38 @@
 // })
 
 
-// /****** 开元棋牌的内部方法*/
-// //1,组装获取请求的url
-// function getURL(s, param) {
-//     let time = new moment().utcOffset(8)
-//     let timestamp = time.unix() * 1000
-//     let url = s != 6 ? config.ky.apiUrl : config.ky.recordUrl
-//     url = url + "?" + qs.stringify({
-//         agent: config.ky.agent,
-//         timestamp: timestamp,
-//         param: desEncode(config.ky.desKey, param),
-//         key: crypto.createHash('md5').update(config.ky.agent + timestamp.toString() + config.ky.md5key).digest('hex'),
-//     })
-//     return url
-// }
-// //2,DES解密
-// function desDecode(desKey, data) {
-//     var cipherChunks = [];
-//     var decipher = crypto.createDecipheriv('aes-128-ecb', desKey, '');
-//     decipher.setAutoPadding(true);
-//     cipherChunks.push(decipher.update(data, 'base64', 'utf8'));
-//     cipherChunks.push(decipher.final('utf8'));
-//     return cipherChunks.join('');
-// }
-// //3,DES加密
-// function desEncode(desKey, data) {
-//     var cipherChunks = [];
-//     var cipher = crypto.createCipheriv('aes-128-ecb', desKey, '');
-//     cipher.setAutoPadding(true);
-//     cipherChunks.push(cipher.update(data, 'utf8', 'base64'));
-//     cipherChunks.push(cipher.final('base64'));
+/****** 开元棋牌的内部方法*/
+//1,组装获取请求的url
+function getURL(s, param) {
+    let time = new moment().utcOffset(8)
+    let timestamp = time.unix() * 1000
+    let url = s != 6 ? config.ky.apiUrl : config.ky.recordUrl
+    url = url + "?" + qs.stringify({
+        agent: config.ky.agent,
+        timestamp: timestamp,
+        param: desEncode(config.ky.desKey, param),
+        key: crypto.createHash('md5').update(config.ky.agent + timestamp.toString() + config.ky.md5key).digest('hex'),
+    })
+    return url
+}
+//2,DES解密
+function desDecode(desKey, data) {
+    var cipherChunks = [];
+    var decipher = crypto.createDecipheriv('aes-128-ecb', desKey, '');
+    decipher.setAutoPadding(true);
+    cipherChunks.push(decipher.update(data, 'base64', 'utf8'));
+    cipherChunks.push(decipher.final('utf8'));
+    return cipherChunks.join('');
+}
+//3,DES加密
+function desEncode(desKey, data) {
+    var cipherChunks = [];
+    var cipher = crypto.createCipheriv('aes-128-ecb', desKey, '');
+    cipher.setAutoPadding(true);
+    cipherChunks.push(cipher.update(data, 'utf8', 'base64'));
+    cipherChunks.push(cipher.final('base64'));
 
-//     return cipherChunks.join('');
-// }
+    return cipherChunks.join('');
+}
 
-// module.exports = router
+module.exports = router
