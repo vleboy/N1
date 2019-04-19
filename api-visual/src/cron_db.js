995c7f9f16e91f468d07bf2e8125745a77d152cf
cheneyweb <457299596@qq.com>
2019-04-20 01:19:35 +0800

    增加用户载入

--- api-visual/src/cron_db.js
+++ api-visual/src/cron_db.js
@@ -125,7 +125,7 @@ cron.schedule('*/30 * * * * *', async () => {
 })
 
 // 玩家定时服务
-cron.schedule('0 20 1 * * *', async () => {
+cron.schedule('0 0 1 * * *', async () => {
     console.time('【玩家载入】')
     let configArr = await nodebatis.query('config.findOne', { type: 'queryTime' })
     let startTime = configArr[0].playerCreatedAt
