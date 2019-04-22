2ed97cf18ef02dfab28d05089cfc7cc6d8299123
xj <799137494@qq.com>
2019-04-22 11:14:37 +0800

    玩家排行榜

--- api-visual/src/api_rank.js
+++ api-visual/src/api_rank.js
@@ -40,15 +40,40 @@ router.get('/rank/merchant', async (ctx, next) => {
     console.timeEnd('商户排行榜统计耗时')
 })
 
-
-
+/**
+ *  玩家排行榜统计
+ */
+router.get('/rank/player', async (ctx, next) => {
+    console.time('商户排行榜统计耗时')
+    let inparam = ctx.request.query
+    //初始map
+    let rankMap = {
+        betCount: [],
+        betAmount: [],
+        retAmount: [],
+        winloseAmount: []
+    }
+    let promiseArr = []
+    // 获取区域玩家总下注次数
+    promiseArr.push(queryGetRank('bill.handleAmountPlayerRank', 'betCount', inparam, rankMap, 3))
+    // 获取区域玩家总下注金额
+    promiseArr.push(queryGetRank('bill.handleAmountPlayerRank', 'betAmount', inparam, rankMap, 3))
+    // 获取区域玩家总返奖
+    promiseArr.push(queryGetRank('bill.handleAmountPlayerRank', 'retAmount', inparam, rankMap, 4))
+    // 获取区域玩家总输赢
+    promiseArr.push(queryGetRank('bill.handleAmountPlayerRank', 'winloseAmount', inparam, rankMap))
+    // 并发执行
+    await Promise.all(promiseArr)
+    ctx.body = { code: 0, data: rankMap }
+    console.timeEnd('商户排行榜统计耗时')
+})
 
 
 // 排行榜统计sql
 async function queryGetRank(sqlName, key, inparam, map, type) {
     let res = await nodebatis.query(sqlName, { method: key, type, ...inparam })
     for (let item of res) {
-        map[key].push({ x: item.parentDisplayName, y: key == 'betAmount' ? Math.abs(item.num) : item.num })
+        map[key].push({ x: item.parentDisplayName || item.userName, y: key == 'betAmount' ? Math.abs(item.num) : item.num })
     }
 }
 
