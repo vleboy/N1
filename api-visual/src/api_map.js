d8372ff2fcad2fe5609fa740e8646667c4edc659
cheneyweb <457299596@qq.com>
2019-04-26 18:37:54 +0800

    优化

--- api-visual/src/api_map.js
+++ api-visual/src/api_map.js
@@ -90,7 +90,7 @@ function getSplitListForWinlose(map, splitCount) {
     let max = _.max(arr)
     let min = _.min(arr)
 
-    let splitList = [{ start: min, end: 0 }]
+    let splitList = [{ start: min, end: -10001 }]
     splitList.push({ start: 0, end: 0 })
     splitList.push({ start: 0, end: max })
     return splitList
