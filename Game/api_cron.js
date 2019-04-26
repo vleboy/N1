4df11717864e3b253cd26dce6d71699c589bd54a
xj <799137494@qq.com>
2019-04-26 11:04:12 +0800

    开元棋牌修复程序

--- Game/api_cron.js
+++ Game/api_cron.js
@@ -8,7 +8,7 @@ const HeraGameRecordModel = require('./model/HeraGameRecordModel')
 module.exports.cronRound = async (e, c, cb) => {
     try {
         //1,获取入参
-        const inparam = JSON.parse(e.body)
+        const inparam = e.body
         // 业务操作
         if (inparam.startTime) {
             if (inparam.startTime + 1 * 60 * 60 * 1000 <= inparam.endTime) {
