8d93e7b24c4f8a3809e6565cd4397ecdfe1bd98d
cheneyweb <457299596@qq.com>
2019-05-08 12:13:00 +0800

    优化

--- up-n1/src/model/PlayerModel.js
+++ up-n1/src/model/PlayerModel.js
@@ -121,12 +121,15 @@ module.exports = class PlayerModel extends BaseModel {
             ScanIndexForward: false,
             Limit: 100,
             ProjectionExpression: ["userId", "userName", "msn", "buId", "merchantName", "nickname", "#state", "gameState", "balance", "joinTime", "gameId", "parent", "parentName", "chip", "createdAt"].join(","),
-            ExpressionAttributeNames: { "#msn": "msn", "#state": "state" }
+            ExpressionAttributeNames: { "#state": "state" }
         }
         this.buildParms(oldscan, conditions)
         if (inparam.startKey) {
             oldscan.ExclusiveStartKey = inparam.startKey;
         }
+        console.log('测试1')
+        console.log(oldscan)
+        console.log('测试2')
         return await this.forScanRes(oldscan, [], inparam.pageSize)
     }
     forScanRes(opts, array = [], pageSize = 20) {
