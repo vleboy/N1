be15f2d4875c5e8c2dd2a1796ba36295300df7d9
cheneyweb <457299596@qq.com>
2019-05-13 11:00:17 +0800

    构建优化

--- api-ttg/src/api_autodeploy.js
+++ api-ttg/src/api_autodeploy.js
@@ -21,6 +21,7 @@ router.post('/deploy/n1web', async function (ctx, next) {
     try {
         log.info('接受到请求，准备持续构建 ...')
         await gitPull('/usr/dev/N1_WEB')
+        await gitPull('/usr/dev/N1_WEB2')
         await deployWebAdmin()
         await deployWebMerchant()
         deployWebManager()
