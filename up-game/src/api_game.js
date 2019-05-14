6fa42c027cc796fd17967e61492bbfc24a2ef24c
cheneyweb <457299596@qq.com>
2019-05-14 10:52:48 +0800

    游戏系统迁移

--- up-game/src/api_game.js
+++ up-game/src/api_game.js
@@ -11,11 +11,11 @@ const log = require('tracer').colorConsole({ level: config.log.level })
 // 持久层相关
 const UserModel = require('./model/UserModel')
 const LogModel = require('./model/LogModel')
-const CompanyModel = require('./model/CompanyModel')
 const GameModel = require('./model/GameModel')
 const GameCheck = require('./biz/GameCheck')
 const RoleCodeEnum = require('./lib/Consts').RoleCodeEnum
 const GameTypeEnum = require('./lib/Consts').GameTypeEnum
+const GameListEnum = require('./lib/Consts').GameListEnum
 
 /**
  * 创建游戏
@@ -114,29 +114,6 @@ router.post('/gameChangeOrder', async function (ctx, next) {
   ctx.body = { code: 0, payload: ret }
 })
 
-// 通过玩家id获取商户的游戏列表(70000/80000/90000)
-router.get('/player/gameList/:userId', async function (ctx, next) {
-  //1,获取入参
-  const userId = ctx.params.userId
-  //2.请求
-  try {
-    let res = await axios.get(`https://${config.env.N1_CENTER}/player/gameList/${userId}`)
-    if (res.data.code != -1) {
-      let resArr = []
-      for (let gameType of res.data) {
-        resArr.push(await new GameModel().list({ gameType: gameType.toString(), query: { gameStatus: 1 } }))
-      }
-      //3.返回结果
-      ctx.body = _.flatten(resArr)
-    } else {
-      ctx.body = { code: res.data.code, msg: res.data.msg }
-    }
-  } catch (error) {
-    console.log(error)
-    ctx.body = { code: -1, msg: 'N' }
-  }
-})
-
 /**
  * 游戏类别
  */
@@ -164,6 +141,28 @@ router.post('/gameType', async function (ctx, next) {
   ctx.body = { code: 0, payload: ret.gameList }
 })
 
+// 通过玩家id获取商户的游戏列表(70000/80000/90000)
+router.get('/player/gameList/:userId', async function (ctx, next) {
+  //1,获取入参
+  const userId = ctx.params.userId
+  //2.请求
+  try {
+    let res = await axios.get(`https://${config.env.N1_CENTER}/player/gameList/${userId}`)
+    if (res.data.code != -1) {
+      let resArr = []
+      for (let gameType of res.data) {
+        resArr.push(await new GameModel().list({ gameType: gameType.toString(), query: { gameStatus: 1 } }))
+      }
+      //3.返回结果
+      ctx.body = _.flatten(resArr)
+    } else {
+      ctx.body = { code: res.data.code, msg: res.data.msg }
+    }
+  } catch (error) {
+    console.log(error)
+    ctx.body = { code: -1, msg: 'N' }
+  }
+})
 // 为包站系统定制使用帐号查询
 router.post('/companySelect', async function (ctx, next) {
   let inparam = ctx.request.body
@@ -181,18 +180,12 @@ router.post('/companySelect', async function (ctx, next) {
 router.post('/companySelect', async function (ctx, next) {
   let inparam = ctx.request.body
   let gameTypeArr = []
-  const companyRet = await new CompanyModel().scan({
-    ProjectionExpression: 'companyIden,companyName'
-  })
   // 管理员或上级是管理员，则获取全部游戏类别
   if (!inparam.parent || inparam.parent == RoleCodeEnum.PlatformAdmin || inparam.parent == '01') {
-    for (let item of companyRet.Items) {
-      gameTypeArr.push({ company: item.companyIden, companyName: item.companyName })
-    }
-    return ctx.body = { code: 0, payload: gameTypeArr }
+    return ctx.body = { code: 0, payload: CompanyEnum }
   }
   // 否则只查询上级有的游戏
-  const userRet = await new UserModel().queryUserGameInfo(inparam.parent)
+  const userRet = await new UserModel().queryUserById(inparam.parent, { ProjectionExpression: "gameList" })
   let parentGameList = userRet.gameList || []
   // 刷新最新游戏类型内容
   let newGameList = []
@@ -202,8 +195,8 @@ router.post('/companySelect', async function (ctx, next) {
   //去重
   newGameList = _.uniqWith(newGameList, _.isEqual)
   for (let item of newGameList) {
-    for (let company of companyRet.Items) {
-      if (item.company == company.companyIden) {
+    for (let company of CompanyEnum) {
+      if (item.company == company.company) {
         gameTypeArr.push({ company: item.company, companyName: company.companyName })
       }
     }
@@ -228,41 +221,20 @@ router.post('/gameBigType', async function (ctx, next) {
   if (!inparam.companyIden) {
     return ctx.body = { code: 0, payload: [] }
   }
-  // 获取全部的游戏大类
   let gameTypeArr = []
+  // 获取全部的游戏大类
   if (inparam.companyIden == '-1') {
-    for (let item in GameTypeEnum) {
-      gameTypeArr.push(GameTypeEnum[item])
-    }
-    return ctx.body = { code: 0, payload: gameTypeArr }
+    gameTypeArr = Object.values(GameTypeEnum)
   }
-  // 获取指定运营商的游戏大类
-  let ret = await new CompanyModel().scan({
-    ProjectionExpression: 'gameTypeList',
-    FilterExpression: "companyIden=:companyIden",
-    ExpressionAttributeValues: {
-      ':companyIden': inparam.companyIden
-    }
-  })
-  if (ret.Items && ret.Items.length != 0) {
-    let typeArr = ret.Items[0].gameTypeList
-    // 没有userId，直接返回
-    if (!inparam.userId) {
-      for (let item of typeArr) {
-        gameTypeArr.push(GameTypeEnum[item])
+  // 获取指定商户的游戏大类
+  else {
+    if (inparam.userId) {
+      let userRet = await new UserModel().queryUserById(inparam.parent, { ProjectionExpression: "gameList" })
+      for (let item of userRet.gameList) {
+        gameTypeArr.push(GameTypeEnum[item.code])
       }
-      return ctx.body = { code: 0, payload: gameTypeArr }
-    }
-    //如果有userId过滤掉没有的游戏大类
-    let userRet = await new UserModel().queryUserGameInfo(inparam.userId)
-    let newTypeArr = []
-    for (let i = 0; i < userRet.gameList.length; i++) {
-      if (_.includes(typeArr, userRet.gameList[i].code)) {
-        newTypeArr.push(userRet.gameList[i].code)
-      }
-    }
-    for (let item of newTypeArr) {
-      gameTypeArr.push(GameTypeEnum[item])
+    } else {
+      gameTypeArr = GameListEnum[inparam.companyIden]
     }
   }
   // 结果返回
--- up-game/src/lib/Consts.js
+++ up-game/src/lib/Consts.js
@@ -25,6 +25,61 @@ const GameTypeEnum = {
     '1160000': { type: 2, code: '1160000', name: 'PP电子游戏', company: 'PP' },
 }
 
+const GameListEnum = {
+    "NA": [
+        // { company: 'NA', code: '3', name: 'NA商城' },
+        { company: 'NA', code: '10000', name: 'NA棋牌游戏', type: 4 },
+        { company: 'NA', code: '30000', name: 'NA真人视讯', type: 1 },
+        { company: 'NA', code: '40000', name: 'NA电子游戏', type: 2 },
+        { company: 'NA', code: '50000', name: 'NA街机游戏', type: 3 },
+        { company: 'NA', code: '60000', name: 'NA捕鱼游戏', type: 6 },
+        { company: 'NA', code: '70000', name: 'H5电子游戏', type: 2 },
+        { company: 'NA', code: '80000', name: 'H5真人视讯', type: 1 },
+        { company: 'NA', code: '90000', name: 'H5电子游戏-无神秘奖', type: 2 }
+    ],
+    "TTG": [
+        { company: 'TTG', code: '1010000', name: 'TTG电子游戏', type: 2 }
+    ],
+    "PNG": [
+        { company: 'PNG', code: '1020000', name: 'PNG电子游戏', type: 2 },
+    ],
+    "MG": [
+        { company: 'MG', code: '10300000', name: 'MG电子游戏', type: 2 }
+    ],
+    "HABA": [
+        { company: 'HABA', code: '1040000', name: 'HABA电子游戏', type: 2 },
+    ],
+    "AG": [
+        { company: 'AG', code: '1050000', name: 'AG真人游戏', type: 1 }
+    ],
+    "SA": [
+        { company: 'SA', code: '1060000', name: 'SA真人游戏', type: 1 },
+        { company: 'SA', code: '1110000', name: 'SA捕鱼游戏', type: 6 }
+    ],
+    "KY": [
+        { company: 'KY', code: '1070000', name: 'KY棋牌游戏', type: 4 }
+    ],
+    "PG": [
+        { company: 'PG', code: '1090000', name: 'PG电子游戏', type: 2 }
+    ],
+    "YSB": [
+        { company: 'YSB', code: '1130000', name: 'YSB体育游戏', type: 5 }
+    ],
+    "RTG": [
+        { company: 'RTG', code: '1140000', name: 'RTG电子游戏', type: 2 }
+    ],
+    "SB": [
+        { company: 'SB', code: '1080000', name: 'SB电子游戏', type: 2 },
+        { company: 'SB', code: '1120000', name: 'SB真人游戏', type: 1 }
+    ],
+    "DT": [
+        { company: 'DT', code: '1150000', name: 'DT电子游戏', type: 2 },
+    ],
+    "PP": [
+        { company: 'PP', code: '1160000', name: 'PP电子游戏', type: 2 },
+    ]
+}
+
 const RoleCodeEnum = {
     'SuperAdmin': '0',
     'PlatformAdmin': '1',
@@ -64,6 +119,7 @@ const SeatTypeEnum = {
 
 module.exports = {
     GameTypeEnum,
+    GameListEnum,
     RoleCodeEnum,
     CompanyStatusEnum,
     GameStatusEnum,
