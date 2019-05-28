db25bf05b3b840d4c6eba410d06e485df1e8ae68
cheneyweb <457299596@qq.com>
2019-05-29 00:20:08 +0800

    迁移风控中心

--- api-visual/src/cron_db.js
+++ api-visual/src/cron_db.js
@@ -61,7 +61,7 @@ async function queryIp(ip) {
 }
 
 // 流水定时服务
-cron.schedule('*/3 * * * * *', async () => {
+cron.schedule('*/30 * * * * *', async () => {
     console.time('【流水载入】')
     let configArr = await nodebatis.query('config.findOne', { type: 'queryTime' })
     if (configArr[0].flag || Date.now() - configArr[0].createdAt > 5 * 60 * 1000) {
