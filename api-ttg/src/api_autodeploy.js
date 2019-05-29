5d7884f371c8eef3c062630a75370e4b1fc4aaee
cheneyweb <457299596@qq.com>
2019-05-29 14:49:02 +0800

    错误码完善

--- api-ttg/src/api_autodeploy.js
+++ api-ttg/src/api_autodeploy.js
@@ -17,7 +17,7 @@ const execOptions = {
 }
 
 // 部署重构后台前端
-router.post('/deploy/n1web', async function (ctx, next) {
+router.post('/deploy/n1web', async (ctx, next) => {
     try {
         log.info('接受到请求，准备持续构建 ...')
         await gitPull('/usr/dev/N1_WEB2')
