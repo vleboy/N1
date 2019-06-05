aca48e237deda35a80cc6b3a281aadd307889157
cheneyweb <457299596@qq.com>
2019-06-05 09:59:50 +0800

    自动构建

--- api-ttg/src/api_autodeploy.js
+++ api-ttg/src/api_autodeploy.js
@@ -34,72 +34,6 @@ router.post('/deploy/n1web', async (ctx, next) => {
         ctx.body = 'N'
     }
 })
-// 部署游戏网页
-router.post('/deploy/ttg', async function (ctx, next) {
-    try {
-        log.info('开始自动构建【第三方网页游戏】...')
-        const commands = [
-            'cd /usr/dev/NA_WEB/webgame',
-            'git pull',
-            // 亚马逊：第三方网页游戏静态资源放置
-            '/usr/local/bin/aws s3 rm s3://ext.na77.org/*',
-            '/usr/local/bin/aws s3 sync . s3://ext.na77.org --acl public-read --delete',
-            // 阿里云：第三方网页游戏静态资源放置
-            '/usr/dev/AWS_Platform/api-ttg/ossutil cp -rf . oss://webrisheng3d/',
-        ].join(' && ')
-
-        // '\cp -f /usr/dev/NA_WEB/webgame/ttg/index.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/ttg/ttg_detail.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/sa/sa.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/mg/mg.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/mg/mg_detail.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/rtg/rtg.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/dt/dt.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/dt/dt_detail.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/sb/sb_video.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/sb/sb_live.html .',
-        // '\cp -f /usr/dev/NA_WEB/webgame/sb/sb_detail.html .',
-
-        // 'npm run build-test-aws',
-        // 'cd dist',
-
-        // // TTG阿里云
-        // 'cd /usr/dev/NA_WEB/ttg',
-        // 'npm run build-test-aliyun',
-        // 'cd dist',
-
-        // // MG亚马逊
-        // 'cd /usr/dev/NA_WEB/mg',
-        // 'npm run build-test-aws',
-        // 'cd dist',
-        // '/usr/local/bin/aws s3 sync . s3://ext.na77.org --acl public-read',
-
-        // // MG阿里云
-        // 'cd /usr/dev/NA_WEB/mg',
-        // 'npm run build-test-aliyun',
-        // 'cd dist',
-        // '/usr/dev/AWS_Platform/api-ttg/ossutil cp -rf . oss://webrisheng3d/'
-        deploy(commands)
-        ctx.body = 'Y'
-    } catch (error) {
-        log.error('自动构建【网页游戏】发生错误异常：')
-        log.error(error)
-        ctx.body = 'N'
-    }
-})
-// 部署旧后台前端
-// router.post('/deploy/na', async function (ctx, next) {
-//     try {
-//         log.info('接受到请求，准备持续构建 ...')
-//         await gitPull('/usr/dev/NA/')
-//         deployGame()
-//         ctx.body = 'Y'
-//     } catch (error) {
-//         log.error('自动构建发生错误异常：')
-//         log.error(error)
-//         ctx.body = 'N'
-//     }
-// })
 function gitPull(path) {
     return new Promise((reslove, reject) => {
         log.info('进入目录：' + path)
@@ -123,7 +57,6 @@ function gitPull(path) {
         })
     })
 }
-
 // 部署函数
 function deployWebAdmin() {
     return new Promise((reslove, reject) => {
@@ -135,31 +68,6 @@ function deployWebAdmin() {
             '/usr/local/bin/aws s3 sync . s3://dev-admin.na12345.com --acl public-read --delete',
         ].join(' && ')
         log.info('开始自动构建N1_WEB平台管理员系统 ...')
-        exec(commands, execOptions, (error, stdout, stderr) => {
-            if (error) {
-                log.error(`exec error: ${error}`)
-                reject(error)
-            }
-            if (stdout) {
-                log.info(`stdout: ${stdout}`)
-            }
-            if (stderr) {
-                log.error(`stderr: ${stderr}`)
-            }
-            reslove(stdout)
-        })
-    })
-}
-function deployWebAgent() {
-    return new Promise((reslove, reject) => {
-        const commands = [
-            'cd /usr/dev/N1_WEB2/n1-agent',
-            'npm run test',
-            'cd agent',
-            '/usr/local/bin/aws s3 rm s3://dev-agent.na12345.com/*',
-            '/usr/local/bin/aws s3 sync . s3://dev-agent.na12345.com --acl public-read --delete',
-        ].join(' && ')
-        log.info('开始自动构建N1_WEB平台代理系统 ...')
         exec(commands, execOptions, (error, stdout, stderr) => {
             if (error) {
                 log.error(`exec error: ${error}`)
@@ -225,16 +133,16 @@ function deployWebManager() {
         })
     })
 }
-function deployWebGame() {
+function deployWebAgent() {
     return new Promise((reslove, reject) => {
         const commands = [
-            'cd /usr/dev/N1_WEB/n1-game',
-            'npm run test-game',
-            'cd game',
-            '/usr/local/bin/aws s3 rm s3://dev-game.na12345.com/*',
-            '/usr/local/bin/aws s3 sync . s3://dev-game.na12345.com --acl public-read --delete',
+            'cd /usr/dev/N1_WEB2/n1-agent',
+            'npm run test',
+            'cd agent',
+            '/usr/local/bin/aws s3 rm s3://dev-agent.na12345.com/*',
+            '/usr/local/bin/aws s3 sync . s3://dev-agent.na12345.com --acl public-read --delete',
         ].join(' && ')
-        log.info('开始自动构建游戏系统 ...')
+        log.info('开始自动构建N1_WEB平台代理系统 ...')
         exec(commands, execOptions, (error, stdout, stderr) => {
             if (error) {
                 log.error(`exec error: ${error}`)
@@ -250,19 +158,113 @@ function deployWebGame() {
         })
     })
 }
-function deploy(commands) {
-    exec(commands, execOptions, (error, stdout, stderr) => {
-        if (error) {
-            log.error(`exec error: ${error}`)
-            return
-        }
-        if (stdout) {
-            log.info(`stdout: ${stdout}`)
-        }
-        if (stderr) {
-            log.error(`stderr: ${stderr}`)
-        }
-        log.info('结束自动构建')
-    })
-}
+
+
+// // 部署游戏网页
+// router.post('/deploy/ttg', async function (ctx, next) {
+//     try {
+//         log.info('开始自动构建【第三方网页游戏】...')
+//         const commands = [
+//             'cd /usr/dev/NA_WEB/webgame',
+//             'git pull',
+//             // 亚马逊：第三方网页游戏静态资源放置
+//             '/usr/local/bin/aws s3 rm s3://ext.na77.org/*',
+//             '/usr/local/bin/aws s3 sync . s3://ext.na77.org --acl public-read --delete',
+//             // 阿里云：第三方网页游戏静态资源放置
+//             '/usr/dev/AWS_Platform/api-ttg/ossutil cp -rf . oss://webrisheng3d/',
+//         ].join(' && ')
+
+//         // '\cp -f /usr/dev/NA_WEB/webgame/ttg/index.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/ttg/ttg_detail.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/sa/sa.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/mg/mg.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/mg/mg_detail.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/rtg/rtg.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/dt/dt.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/dt/dt_detail.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/sb/sb_video.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/sb/sb_live.html .',
+//         // '\cp -f /usr/dev/NA_WEB/webgame/sb/sb_detail.html .',
+
+//         // 'npm run build-test-aws',
+//         // 'cd dist',
+
+//         // // TTG阿里云
+//         // 'cd /usr/dev/NA_WEB/ttg',
+//         // 'npm run build-test-aliyun',
+//         // 'cd dist',
+
+//         // // MG亚马逊
+//         // 'cd /usr/dev/NA_WEB/mg',
+//         // 'npm run build-test-aws',
+//         // 'cd dist',
+//         // '/usr/local/bin/aws s3 sync . s3://ext.na77.org --acl public-read',
+
+//         // // MG阿里云
+//         // 'cd /usr/dev/NA_WEB/mg',
+//         // 'npm run build-test-aliyun',
+//         // 'cd dist',
+//         // '/usr/dev/AWS_Platform/api-ttg/ossutil cp -rf . oss://webrisheng3d/'
+//         deploy(commands)
+//         ctx.body = 'Y'
+//     } catch (error) {
+//         log.error('自动构建【网页游戏】发生错误异常：')
+//         log.error(error)
+//         ctx.body = 'N'
+//     }
+// })
+// function deploy(commands) {
+//     exec(commands, execOptions, (error, stdout, stderr) => {
+//         if (error) {
+//             log.error(`exec error: ${error}`)
+//             return
+//         }
+//         if (stdout) {
+//             log.info(`stdout: ${stdout}`)
+//         }
+//         if (stderr) {
+//             log.error(`stderr: ${stderr}`)
+//         }
+//         log.info('结束自动构建')
+//     })
+// }
+// 部署旧后台前端
+// router.post('/deploy/na', async function (ctx, next) {
+//     try {
+//         log.info('接受到请求，准备持续构建 ...')
+//         await gitPull('/usr/dev/NA/')
+//         deployGame()
+//         ctx.body = 'Y'
+//     } catch (error) {
+//         log.error('自动构建发生错误异常：')
+//         log.error(error)
+//         ctx.body = 'N'
+//     }
+// })
+// function deployWebGame() {
+//     return new Promise((reslove, reject) => {
+//         const commands = [
+//             'cd /usr/dev/N1_WEB/n1-game',
+//             'npm run test-game',
+//             'cd game',
+//             '/usr/local/bin/aws s3 rm s3://dev-game.na12345.com/*',
+//             '/usr/local/bin/aws s3 sync . s3://dev-game.na12345.com --acl public-read --delete',
+//         ].join(' && ')
+//         log.info('开始自动构建游戏系统 ...')
+//         exec(commands, execOptions, (error, stdout, stderr) => {
+//             if (error) {
+//                 log.error(`exec error: ${error}`)
+//                 reject(error)
+//             }
+//             if (stdout) {
+//                 log.info(`stdout: ${stdout}`)
+//             }
+//             if (stderr) {
+//                 log.error(`stderr: ${stderr}`)
+//             }
+//             reslove(stdout)
+//         })
+//     })
+// }
+
 module.exports = router
