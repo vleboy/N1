4374f9279efc1e9e8d4ca327867120d8cd2290aa
cheneyweb <457299596@qq.com>
2019-07-18 11:38:02 +0800

    N1

--- Game/up-game/src/api_company.js
+++ Game/up-game/src/api_company.js
@@ -8,27 +8,11 @@ const _ = require('lodash')
 // 日志相关
 const log = require('tracer').colorConsole({ level: config.log.level })
 // 持久层相关
-const CompanyModel = require('./model/CompanyModel')
-const LogModel = require('./model/LogModel')
-const CompanyCheck = require('./biz/CompanyCheck')
+// const CompanyModel = require('./model/CompanyModel')
+// const LogModel = require('./model/LogModel')
+// const CompanyCheck = require('./biz/CompanyCheck')
 const BizErr = require('./lib/Codes').BizErr
 const CompanyEnum = require('./lib/Consts').companyEnum
-
-// 创建厂商
-// router.post('/companyNew', async function (ctx, next) {
-//     let companyInfo = ctx.request.body
-//     let token = ctx.tokenVerify
-//     //检查参数是否合法
-//     new CompanyCheck().checkCompany(companyInfo)
-//     // 业务操作
-//     const addCompanyRet = await new CompanyModel().addCompany(companyInfo)
-//     // 操作日志记录
-//     companyInfo.operateAction = '创建厂商'
-//     companyInfo.operateToken = token
-//     new LogModel().addOperate(companyInfo, null, addCompanyRet)
-//     // 结果返回
-//     ctx.body = { code: 0, payload: addCompanyRet }
-// })
 
 //  厂商列表
 router.post('/companyList', async function (ctx, next) {
@@ -55,6 +39,39 @@ router.get('/companyOne/:companyName/:companyId', async function (ctx, next) {
     // 结果返回
     ctx.body = { code: 0, payload: ret }
 })
+
+// 通过厂商名称获取单个厂商
+router.get('/companyOne/:companyName', async function (ctx, next) {
+    let inparam = ctx.params
+    let token = ctx.tokenVerify
+    // 参数校验
+    if (!inparam.companyName) {
+        throw BizErr.InparamErr()
+    } else {
+        inparam.companyName = decodeURIComponent(inparam.companyName)
+    }
+    // 业务操作
+    // const ret = await new CompanyModel().getOne(inparam)
+    const ret = _.find(companyEnum, (o) => { return o.companyName == inparam.companyName })
+    // 结果返回
+    ctx.body = { code: 0, payload: ret }
+})
+
+// 创建厂商
+// router.post('/companyNew', async function (ctx, next) {
+//     let companyInfo = ctx.request.body
+//     let token = ctx.tokenVerify
+//     //检查参数是否合法
+//     new CompanyCheck().checkCompany(companyInfo)
+//     // 业务操作
+//     const addCompanyRet = await new CompanyModel().addCompany(companyInfo)
+//     // 操作日志记录
+//     companyInfo.operateAction = '创建厂商'
+//     companyInfo.operateToken = token
+//     new LogModel().addOperate(companyInfo, null, addCompanyRet)
+//     // 结果返回
+//     ctx.body = { code: 0, payload: addCompanyRet }
+// })
 
 // // 厂商状态变更，接口编号：
 // router.post('/companyChangeStatus', async function (ctx, next) {
@@ -86,23 +103,5 @@ router.get('/companyOne/:companyName/:companyId', async function (ctx, next) {
 //     // 结果返回
 //     ctx.body = { code: 0, payload: ret }
 // })
-
-// 通过厂商名称获取单个厂商
-router.get('/companyOne/:companyName', async function (ctx, next) {
-    let inparam = ctx.params
-    let token = ctx.tokenVerify
-    // 参数校验
-    if (!inparam.companyName) {
-        throw BizErr.InparamErr()
-    } else {
-        inparam.companyName = decodeURIComponent(inparam.companyName)
-    }
-    // 业务操作
-    // const ret = await new CompanyModel().getOne(inparam)
-    const ret = _.find(companyEnum, (o) => { return o.companyName == inparam.companyName })
-    // 结果返回
-    ctx.body = { code: 0, payload: ret }
-})
-
 
 module.exports = router
