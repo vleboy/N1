ebba7053e48e9dcd8a8e958e2f9a3712d010267f
cheneyweb <457299596@qq.com>
2019-07-31 15:55:01 +0800

    VG

--- api-ttg/src/api_vg.js
+++ api-ttg/src/api_vg.js
@@ -6,6 +6,7 @@ const router = new Router()
 // 工具相关
 const _ = require('lodash')
 const axios = require('axios')
+var querystring = require("querystring")
 const CryptoJS = require("crypto-js")
 const parseString = require('xml2js').parseString
 // 日志相关
@@ -24,41 +25,43 @@ const ipMap = {}
  */
 router.get('/vg/gameurl/:gameId/:sid/:userId/:token', async (ctx, next) => {
     ipMap[ctx.params.userId] = ctx.request.ip
-    // 请求N1服务器是否允许玩家进入游戏
-    const nares = await axios.post(config.na.joingameurl, {
-        userId: ctx.params.userId,
-        gameId: config.sa.gameType,
-        sid: config.sa.gameId,
-        token: ctx.params.token
-    })
-    if (nares.data.code != 0) {
-        return ctx.body = { code: nares.data.code, msg: nares.data.msg }
-    }
-    // 检查VG玩家注册
-    let player = await new PlayerModel().getPlayerById(ctx.params.userId)
-    if (!player.regMap || !player.regMap.vg) {
-        let verifyCode = CryptoJS.MD5(`${ctx.params.userId}create${config.vg.channel}${config.vg.privatekey}`).toString(CryptoJS.enc.Hex).toUpperCase()
-        let res = await axios.get(`${config.vg.apiUrl}?username=${ctx.params.userId}&action=create&channel=${config.vg.channel}&verifyCode=${verifyCode}`)
-        res = await xmlParse(res.data)
-        if (res.response.errcode[0] == '0' || res.response.errcode[0] == '-99') {
-            player.regMap ? player.regMap.vg = 1 : player.regMap = { vg: 1 }
-            new PlayerModel().updateRegMap(player)
-        } else {
-            return ctx.body = res
-        }
-    }
+    const inparam = ctx.params
+    let res = {}
     // 默认移动版
     let gameversion = ctx.request.query.lobbyType != '0' ? 2 : 1
+    // 正式玩家
+    if (inparam.userId != 0) {
+        // 请求N1服务器是否允许玩家进入游戏
+        const nares = await axios.post(config.na.joingameurl, {
+            userId: inparam.userId,
+            gameId: config.sa.gameType,
+            sid: config.sa.gameId,
+            token: inparam.token
+        })
+        if (nares.data.code != 0) {
+            return ctx.body = { code: nares.data.code, msg: nares.data.msg }
+        }
+        // 检查VG玩家注册
+        let player = await new PlayerModel().getPlayerById(inparam.userId)
+        if (!player.regMap || !player.regMap.vg) {
+            res = await getVG({ username: inparam.userId, action: 'create' })
+            if (res.response.errcode == '0' || res.response.errcode == '-99') {
+                player.regMap ? player.regMap.vg = 1 : player.regMap = { vg: 1 }
+                new PlayerModel().updateRegMap(player)
+            } else {
+                return ctx.body = res
+            }
+        }
+    }
+    // 试玩玩家
+    else {
+        res = await getVG({ gametype: '1000', gameversion })
+    }
     // 请求VG游戏登录
-    let verifyCode = CryptoJS.MD5(`${ctx.params.userId}loginWithChannel${config.vg.channel}1000${gameversion}true${config.vg.privatekey}`).toString(CryptoJS.enc.Hex).toUpperCase()
-    console.log(`请求VG登录MD5加密前：${ctx.params.userId}loginWithChannel${config.vg.channel}1000${gameversion}true${config.vg.privatekey}`)
-    console.log(`请求VG登录MD5加密后：${verifyCode}`)
-    console.log(`请求VG登录URL：${config.vg.apiUrl}?username=${ctx.params.userId}&action=loginWithChannel&channel=${config.vg.channel}&gametype=1000&gameversion=${gameversion}&create=true&verifyCode=${verifyCode}`)
-    let res = await axios.get(`${config.vg.apiUrl}?username=${ctx.params.userId}&action=loginWithChannel&channel=${config.vg.channel}&gametype=1000&gameversion=${gameversion}&create=true&verifyCode=${verifyCode}`)
-    res = await xmlParse(res.data)
-    console.log(res)
+    res = await getVG({ username: inparam.userId, action: 'loginWithChannel', gametype: '1000', gameversion, create: 'true' })
+    log.info(res)
     // 跳转VG游戏
-    ctx.redirect(res.response.result[0])
+    ctx.redirect(res.response.result)
 })
 
 /**
@@ -84,7 +87,7 @@ router.post('/vg/transaction', async (ctx, next) => {
     inparam.businessKey = `BVG_${inparam.username}_${inparam.transactionId}`
     inparam.anotherGameData = JSON.stringify(inparam)
     inparam.txnidTemp = `${inparam.username}_${inparam.type}_${inparam.transactionId}`
-    console.log(inparam)
+    log.info(inparam)
     let amtAfter = await new PlayerModel().updatebalance(player, inparam)
     if (amtAfter == 'err') {
         ctx.body = { code: -1, msg: 'error' }
@@ -93,11 +96,24 @@ router.post('/vg/transaction', async (ctx, next) => {
     }
 })
 
+// 私有方法：VG请求
+async function getVG(obj) {
+    obj.channel = config.vg.channel
+    let query = querystring.stringify(obj)
+    let verifyCode = ''
+    for (let key in obj) {
+        verifyCode += obj[key]
+    }
+    verifyCode = CryptoJS.MD5(`${verifyCode}${config.vg.privatekey}`).toString(CryptoJS.enc.Hex).toUpperCase()
+    let res = await axios.get(`${config.vg.apiUrl}?${query}&verifyCode=${verifyCode}`)
+    res = await xmlParse(res.data)
+    return res
+}
 
 // 私有方法：XML解析
 function xmlParse(xml) {
     return new Promise((reslove, reject) => {
-        parseString(xml, function (err, res) {
+        parseString(xml, { explicitArray: false }, (err, res) => {
             reslove(res)
         })
     })
