ec98fde0acf9bdf7257cfd03d9bac70f76f42997
cheneyweb <457299596@qq.com>
2019-07-31 11:11:40 +0800

    初步增加VG

--- api-ttg/src/api_vg.js
+++ api-ttg/src/api_vg.js
@@ -6,6 +6,7 @@ const router = new Router()
 // 工具相关
 const _ = require('lodash')
 const axios = require('axios')
+const CryptoJS = require("crypto-js")
 const parseString = require('xml2js').parseString
 // 日志相关
 const log = require('tracer').colorConsole({ level: config.log.level })
@@ -19,9 +20,28 @@ const ipMap = {}
  * sid    游戏小类
  * userId 玩家ID
  * token  玩家的NA令牌
+ * lobbyType 0是电脑版1是移动版
  */
 router.get('/vg/gameurl/:gameId/:sid/:userId/:token', async (ctx, next) => {
     ipMap[ctx.params.userId] = ctx.request.ip
+    // 请求N1服务器是否允许玩家进入游戏
+    const nares = await axios.post(config.na.joingameurl, {
+        userId: ctx.params.userId,
+        gameId: config.sa.gameType,
+        sid: config.sa.gameId,
+        token: ctx.params.token
+    })
+    if (nares.data.code != 0) {
+        return ctx.body = { code: nares.data.code, msg: nares.data.msg }
+    }
+    // 默认移动版
+    let gameversion = ctx.request.query.lobbyType != '0' ? 2 : 1
+    // 请求VG游戏登录
+    let verifyCode = CryptoJS.MD5(`${ctx.params.userId}loginWithChannel${config.vg.channel}1000${gameversion}true${config.vg.privatekey}`).toString(CryptoJS.enc.Hex)
+    let finalUrl = await axios.get(`http://<server>/webapi/interface.aspx?username=${ctx.params.userId}&action=loginWithChannel&channel=${config.vg.channel}&gametype=1000&gameversion=${gameversion}&create=true&verifyCode=${verifyCode}`)
+    // 跳转VG游戏
+    log.info(`VG游戏链接 ${finalUrl}`)
+    ctx.redirect(finalUrl)
 })
 
 /**
@@ -34,6 +54,8 @@ router.get('/vg/gameurl/:gameId/:sid/:userId/:token', async (ctx, next) => {
 router.post('/vg/transaction', async (ctx, next) => {
     ipMap[ctx.params.userId] = ctx.request.ip
     let inparam = ctx.request.body
+    console.log(inparam)
+    
     let player = await new PlayerModel().getPlayerById(inparam.userId)
     if (inparam.type == 'BET') {
         inparam.billType = 3
@@ -47,11 +69,11 @@ router.post('/vg/transaction', async (ctx, next) => {
     inparam.anotherGameData = JSON.stringify(inparam)
     inparam.txnidTemp = `${inparam.username}_${inparam.type}_${inparam.transactionId}`
 
-    let amtAfter = await new PlayerModel().updatebalance(player, inparam)
+    // let amtAfter = await new PlayerModel().updatebalance(player, inparam)
 
-    if (amtAfter == 'err') {
-        ctx.body = { code: -1, msg: 'error' }
-    } else {
-        ctx.body = { code: 0, msg: 'success', balance: amtAfter }
-    }
+    // if (amtAfter == 'err') {
+    //     ctx.body = { code: -1, msg: 'error' }
+    // } else {
+    //     ctx.body = { code: 0, msg: 'success', balance: amtAfter }
+    // }
 })
