3809ac32f2d9c4aaec195b026d215033f2e028b3
cheneyweb <457299596@qq.com>
2019-10-07 21:14:30 +0800

    YSB

--- api-ttg/src/api_ysb.js
+++ api-ttg/src/api_ysb.js
@@ -7,7 +7,6 @@ const router = new Router()
 const _ = require('lodash')
 const axios = require('axios')
 const CryptoJS = require("crypto-js")
-// const jwt = require('jsonwebtoken')
 // const legacy = require('legacy-encoding')
 // 日志相关
 const log = require('tracer').colorConsole({ level: config.log.level })
@@ -94,98 +93,79 @@ router.post('/ysb/postTransfer', async (ctx, next) => {
             }
             // log.info(JSON.stringify(Record))
         }
-        // 查询玩家
-        let n2res = await axios.post(config.n2.apiUrl, { userId: UN, method: 'balance' })
-        if (n2res.data.code != 0) {
-            return ctx.body = getYSBResponse(action, { UN: `NAPL_${UN}`, UID: UN, CC: 0, S: 104, ED: '玩家不存在' })
+        // 预置数据
+        const bill = {
+            prefix: 'YSB',
+            userId: +UN,
+            method: '',
+            type: null,
+            amount: null,
+            betsn: null,
+            bk: TRX,
+            sn: TRX,
+            sourceIP: ipMap[userId],
+            gameType: +config.ysb.gameType,
+            gameId: gameIdMap[userId] ? +gameIdMap[userId] : +config.ysb.gameType,
+            inparam
         }
-        BAL = n2res.data.balance
-        // inparam.userName = player.userName
-        // inparam.userId = player.userId
-        // inparam.gameType = config.ysb.gameType
-        // inparam.roundId = TRX ? `CYSB_${UN}_${TRX}` : inparam.roundId
         // 判断交易类型
+        let syncRes = null
         switch (action) {
             case 'ACCOUNTBALANCE':
-                ctx.body = getYSBResponse(action, { UN: `NAPL_${UN}`, CC, BAL, S })
-                break;
-            case 'BET':
-                inparam.billType = 6                                                        // 设置为冻结
-                inparam.amt = parseFloat(AMT) * -1
-                inparam.businessKey = `BYSB_${UN}_${TRX}`
-                inparam.txnidTemp = `${UN}_BET_${TRX}`
-                BAL = await new PlayerModel().updatebalance(player, inparam)
-                if (BAL == 'err') {
-                    S = 1016
-                    ED = '余额不足'
-                } else if (BAL == player.balance) {
-                    S = 1016
-                    ED = '重复投注'
-                    console.error(`重复投注:${inparam.businessKey}`)
+                // 查询玩家
+                let n2res = await axios.post(config.n2.apiUrl, { userId: UN, method: 'balance' })
+                if (n2res.data.code != 0) {
+                    return ctx.body = getYSBResponse(action, { UN: `NAPL_${UN}`, UID: UN, CC: 0, S: 104, ED: '玩家不存在' })
                 }
-                ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, VID, CC, BAL, S, ED })
+                ctx.body = getYSBResponse(action, { UN: `NAPL_${UN}`, CC, BAL: n2res.data.balance, S })
+                break;
+            // 设置为冻结
+            case 'BET':
+                bill.type = 3
+                bill.method = 'bet'
+                bill.amount = Math.abs(+AMT) * -1
+                syncRes = await syncBill(bill)
+                if (syncRes && !syncRes.err) {
+                    ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, VID, CC, BAL: syncRes.balance, S, ED })
+                }
+                if (syncRes && syncRes.err) {
+                    ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, VID, CC, BAL: syncRes.balance, S: 1016, ED: '余额不足' })
+                }
                 break;
             case 'BETCONFIRM':
-                // 查询出被冻结的金额
-                let freezeRecord = await new PlayerBillDetailModel().getBill(`AYSB_${UN}_BET_${TRX}`)
-                // 如果已经冻结，则先解冻
-                if (freezeRecord.Item && !_.isEmpty(freezeRecord.Item)) {
-                    inparam.billType = 5                                                   // 设置为解冻
-                    inparam.amt = Math.abs(freezeRecord.Item.amount)
-                    inparam.businessKey = `BYSB_${UN}_${TRX}`
-                    inparam.txnidTemp = `${UN}_UNBET_${TRX}`
-                    inparam.betsn = `AYSB_${UN}_BET_${TRX}`
-                    BAL = await new PlayerModel().updatebalance(player, inparam)
-                    player.balance = BAL
-                }
+                // 先解冻
+                bill.type = 5
+                bill.method = 'refund'
+                bill.betsn = TRX
+                syncRes = await syncBill(bill)
                 // 遍历所有下注项，每个下注项对应一条下注
                 for (let record of Record) {
                     if (!record.REFID || isNaN(parseInt(record.REFID))) {
                         continue
                     }
-                    inparam.billType = 3
-                    inparam.amt = parseFloat(record.AMT) * -1
-                    inparam.businessKey = `BYSB_${UN}_${record.REFID}`
-                    inparam.txnidTemp = `${UN}_BETCONFIRM_${record.REFID}`
-                    inparam.anotherGameData = JSON.stringify(record)                      // 将原始游戏信息JSON格式化存储
-                    BAL = await new PlayerModel().updatebalance(player, inparam)
-                    // 下注写入YSB战绩，以便提供实时查询，status==3不会提供给商户查询，PAYOUT后会根据主键覆盖
-                    new HeraGameRecordModel().writeRound({
-                        userId: player.userId,
-                        userName: player.userName,
-                        businessKey: inparam.businessKey,
-                        gameId: inparam.gameType.toString(),
-                        gameType: inparam.gameType,
-                        parent: player.parent,
-                        status: 3,
-                        content: { bet: [], ret: [] },
-                        anotherGameData: inparam.anotherGameData,
-                        createdAt: Date.now()
-                    })
+                    bill.type = 3
+                    bill.method = 'bet'
+                    bill.amount = Math.abs(+record.AMT) * -1
+                    bill.bk = record.REFID
+                    bill.sn = record.REFID
+                    bill.inparam = record
+                    syncRes = await syncBill(bill)
                 }
-                ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, CC, BAL, S })
+                if (syncRes && !syncRes.err) {
+                    ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, CC, BAL: syncRes.balance, S })
+                }
                 break;
             case 'PAYOUT':
-                inparam.billType = 4
-                inparam.amt = parseFloat(PAYAMT)
-                inparam.businessKey = `BYSB_${UN}_${REFID}`
-                inparam.txnidTemp = `${UN}_PAYOUT_${REFID}`
-                inparam.betsn = `AYSB_${UN}_BETCONFIRM_${REFID}`
-                inparam.txnid = TRX
-                // 检查派彩调整是否正确
-                if (!await new PlayerBillDetailModel().ysbPayoutCheck(inparam)) {
-                    S = 1013
-                    ED = '返奖已存在'
-                } else {
-                    BAL = await new PlayerModel().updatebalance(player, inparam)
-                    if (BAL == player.balance) {
-                        S = 1013
-                        ED = '返奖已存在'
-                    } else {
-                        new PlayerModel().addRound(inparam)
-                    }
+                bill.type = 4
+                bill.method = 'win'
+                bill.amount = +PAYAMT
+                bill.bk = REFID
+                bill.sn = REFID
+                bill.betsn = REFID
+                syncRes = await syncBill(bill)
+                if (syncRes && !syncRes.err) {
+                    ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, CC, BAL: syncRes.balance, S, ED })
                 }
-                ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, CC, BAL, S, ED })
                 break;
             default:
                 return
