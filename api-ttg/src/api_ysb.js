1f30fb00af45404d9b5fb5056136faea5dbbe7a3
cheneyweb <457299596@qq.com>
2019-10-07 15:37:28 +0800

    YSB

--- api-ttg/src/api_ysb.js
+++ api-ttg/src/api_ysb.js
@@ -42,13 +42,154 @@ router.post('/ysb/login', async (ctx, next) => {
     let S = 0
     let ED = ''
     // log.info(action)
-    // log.info(JSON.stringify(inparam))
+    log.info(JSON.stringify(inparam))
     for (let prop of inparam.properties) {
         UN = prop.$.name == 'UN' ? prop._.split('_')[1] : UN
         SG = prop.$.name == 'SG' ? prop._ : SG
     }
     if (UN.length == 8) {
         ctx.body = getYSBResponse(action, { UN: `NAPL_${UN}`, UID: UN, CC, S, ED })
+    } else {
+        return next()
+    }
+})
+
+/**
+ * YSB唯一接口
+ */
+router.post('/ysb/postTransfer', async (ctx, next) => {
+    // 获取入参
+    let action = ctx.request.body.request.$.action
+    let inparam = ctx.request.body.request.element[0]
+    let UN = null
+    let AMT = null
+    let TRX = null
+    let VID = null
+    let REFID = null
+    let CC = 'RMB'
+    let BAL = 0
+    let S = 0
+    let ED = ''
+    let PAYAMT = 0
+    let Record = []
+    // log.info(action)
+    log.info(JSON.stringify(inparam))
+    for (let prop of inparam.properties) {
+        UN = prop.$.name == 'UN' ? prop._.split('_')[1] : UN
+        AMT = prop.$.name == 'AMT' ? prop._ : AMT
+        TRX = prop.$.name == 'TRX' ? prop._ : TRX
+        VID = prop.$.name == 'VID' ? prop._ : VID
+        PAYAMT = prop.$.name == 'PAYAMT' ? prop._ : PAYAMT
+        REFID = prop.$.name == 'REFID' ? prop._ : REFID
+    }
+    if (UN.length == 8) {
+        // 转换Record的XML对象为JSON对象
+        if (inparam.Record && inparam.Record.length > 0) {
+            for (let record of inparam.Record) {
+                let obj = {}
+                for (let item of record.properties) {
+                    obj[item.$.name] = item._
+                }
+                Record.push(obj)
+            }
+            // log.info(JSON.stringify(Record))
+        }
+        // 查询玩家
+        let n2res = await axios.post(config.n2.apiUrl, { userId: UN, method: 'balance' })
+        if (!n2res.data.code != 0) {
+            return ctx.body = getYSBResponse(action, { UN: `NAPL_${UN}`, UID: UN, CC: 0, S: 104, ED: '玩家不存在' })
+        }
+        BAL = n2res.data.balance
+        // inparam.userName = player.userName
+        // inparam.userId = player.userId
+        // inparam.gameType = config.ysb.gameType
+        // inparam.roundId = TRX ? `CYSB_${UN}_${TRX}` : inparam.roundId
+        // 判断交易类型
+        switch (action) {
+            case 'ACCOUNTBALANCE':
+                ctx.body = getYSBResponse(action, { UN: `NAPL_${UN}`, CC, BAL, S })
+                break;
+            case 'BET':
+                inparam.billType = 6                                                        // 设置为冻结
+                inparam.amt = parseFloat(AMT) * -1
+                inparam.businessKey = `BYSB_${UN}_${TRX}`
+                inparam.txnidTemp = `${UN}_BET_${TRX}`
+                BAL = await new PlayerModel().updatebalance(player, inparam)
+                if (BAL == 'err') {
+                    S = 1016
+                    ED = '余额不足'
+                } else if (BAL == player.balance) {
+                    S = 1016
+                    ED = '重复投注'
+                    console.error(`重复投注:${inparam.businessKey}`)
+                }
+                ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, VID, CC, BAL, S, ED })
+                break;
+            case 'BETCONFIRM':
+                // 查询出被冻结的金额
+                let freezeRecord = await new PlayerBillDetailModel().getBill(`AYSB_${UN}_BET_${TRX}`)
+                // 如果已经冻结，则先解冻
+                if (freezeRecord.Item && !_.isEmpty(freezeRecord.Item)) {
+                    inparam.billType = 5                                                   // 设置为解冻
+                    inparam.amt = Math.abs(freezeRecord.Item.amount)
+                    inparam.businessKey = `BYSB_${UN}_${TRX}`
+                    inparam.txnidTemp = `${UN}_UNBET_${TRX}`
+                    inparam.betsn = `AYSB_${UN}_BET_${TRX}`
+                    BAL = await new PlayerModel().updatebalance(player, inparam)
+                    player.balance = BAL
+                }
+                // 遍历所有下注项，每个下注项对应一条下注
+                for (let record of Record) {
+                    if (!record.REFID || isNaN(parseInt(record.REFID))) {
+                        continue
+                    }
+                    inparam.billType = 3
+                    inparam.amt = parseFloat(record.AMT) * -1
+                    inparam.businessKey = `BYSB_${UN}_${record.REFID}`
+                    inparam.txnidTemp = `${UN}_BETCONFIRM_${record.REFID}`
+                    inparam.anotherGameData = JSON.stringify(record)                      // 将原始游戏信息JSON格式化存储
+                    BAL = await new PlayerModel().updatebalance(player, inparam)
+                    // 下注写入YSB战绩，以便提供实时查询，status==3不会提供给商户查询，PAYOUT后会根据主键覆盖
+                    new HeraGameRecordModel().writeRound({
+                        userId: player.userId,
+                        userName: player.userName,
+                        businessKey: inparam.businessKey,
+                        gameId: inparam.gameType.toString(),
+                        gameType: inparam.gameType,
+                        parent: player.parent,
+                        status: 3,
+                        content: { bet: [], ret: [] },
+                        anotherGameData: inparam.anotherGameData,
+                        createdAt: Date.now()
+                    })
+                }
+                ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, CC, BAL, S })
+                break;
+            case 'PAYOUT':
+                inparam.billType = 4
+                inparam.amt = parseFloat(PAYAMT)
+                inparam.businessKey = `BYSB_${UN}_${REFID}`
+                inparam.txnidTemp = `${UN}_PAYOUT_${REFID}`
+                inparam.betsn = `AYSB_${UN}_BETCONFIRM_${REFID}`
+                inparam.txnid = TRX
+                // 检查派彩调整是否正确
+                if (!await new PlayerBillDetailModel().ysbPayoutCheck(inparam)) {
+                    S = 1013
+                    ED = '返奖已存在'
+                } else {
+                    BAL = await new PlayerModel().updatebalance(player, inparam)
+                    if (BAL == player.balance) {
+                        S = 1013
+                        ED = '返奖已存在'
+                    } else {
+                        new PlayerModel().addRound(inparam)
+                    }
+                }
+                ctx.body = getYSBResponse(action, { TRX, UN: `NAPL_${UN}`, CC, BAL, S, ED })
+                break;
+            default:
+                return
+        }
     } else {
         return next()
     }
